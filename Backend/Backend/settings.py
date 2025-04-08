2dd9f30f2721938616b6da7064e11858a5b760cd
wassimmho <wassim.mho000@gmail.com>
2025-04-08 22:39:17 +0100

    env2

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -72,7 +72,9 @@ CORS_ALLOW_ALL_ORIGINS = True  # For development only
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://localhost:5174",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
 ]
 
 
